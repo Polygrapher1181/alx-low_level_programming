e75528f385ea5de66103261eab0f6700caac2ba7
Polygrapher1181 <moigowanjohi@gmail.com>
2023-02-21 08:55:23 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char *sh = "_putchar";
+
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+
+return (0);
+}
+
