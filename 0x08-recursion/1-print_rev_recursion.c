574d66809b5805219cc7c8464824b42046586c1b
Polygrapher1181 <moigowanjohi@gmail.com>
2023-03-08 02:34:03 -0800

    #include holberton.h

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- *
- * @s: the string to reverse
- *
- * Return: nothing
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
-
 void _print_rev_recursion(char *s)
 {
-if (*s != '\0')
-{
-_print_rev_recursion(s + 1);
-_putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
-}
-
