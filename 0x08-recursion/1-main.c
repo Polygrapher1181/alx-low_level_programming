2a7ebd7955c3e264ee5977f88ee932525649c3d8
Polygrapher1181 <moigowanjohi@gmail.com>
2023-03-08 02:43:51 -0800

    function that prints a string in reverse

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main -check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    _print_rev_recursion("\nColton Walker");
-    return (0);
+	_print_rev_recursion("\Colton Walker");
+	return (0);
 }
