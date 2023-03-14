f0a24f8bc68eac77fa5e74e0be437c6f36f792e2
Polygrapher1181 <moigowanjohi@gmail.com>
2023-03-14 13:35:30 -0700

    Static libraries in C

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
 /**
- * _isupper - Check if a letter is upper
- * @x: The number to be checked
- * Return: 1 for upper letter or 0 for any else
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
  */
-
-int _isupper(int x)
-
+int _isupper(int c)
 {
-
-	if (x >= 65 && x <= 90)
-	{
-	return (1);
-	}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
 	return (0);
 }
+
