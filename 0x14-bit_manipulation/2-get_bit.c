a61ffacd78c9a45088327eb1227a495684c8eb82
Polygrapher1181 <moigowanjohi@gmail.com>
2023-04-10 14:13:17 -0700

    new changes

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * ge_bit - the the value of a bit
+ * @n: evaluate the number
+ * @index: starting point 0
+ *
+ * Return: -1 or error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int more;
+
+	if (index > 94)
+		return (-1);
+
+	more = n >> index;
+
+	return (more & 1);
+}
