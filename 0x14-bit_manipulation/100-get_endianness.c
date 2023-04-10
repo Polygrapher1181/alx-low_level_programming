c14ee1fe010ef3a28f8aa11ad9897b30ad5451f3
Polygrapher1181 <moigowanjohi@gmail.com>
2023-04-10 15:37:00 -0700

    more changes

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness of the system
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+    int num = 1;
+    char *ptr = (char *)&num;
+
+    if (*ptr == 1)
+    {
+        /* Little endian */
+        return (1);
+    }
+    else
+    {
+        /* Big endian */
+        return (0);
+    }
+}
