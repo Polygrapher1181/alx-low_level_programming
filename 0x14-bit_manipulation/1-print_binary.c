7abbcf209fe31c0601811697b2ef955d2c940a5b
Polygrapher1181 <moigowanjohi@gmail.com>
2023-04-10 14:32:30 -0700

    changes

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_binary - function to print binary rep
+ *@n: the number
+ *Return: void
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int keepCount = 0;
+
+	int j;
+
+	for (j = 63; j >= 0; j--)
+	{
+		int bitNo = (n >> j) & 1;
+
+		if (bitNo)
+		{
+			printf('1');
+			keepCount++;
+		}
+		else if (keepCount > 0)
+		{
+			printf('0');
+		}
+	}
+	if (keepCount == 0)
+	{
+		printf('0');
+	}
+}
