557116eea0426e481026134f25d6df9211dab0fa
Polygrapher1181 <moigowanjohi@gmail.com>
2023-04-10 16:38:24 -0700

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <fcntl.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: A pointer to the name of the file to read.
- * @letters: The number of letters to read and print.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the function should read and print.
  *
- * Return: The number of letters read and printed, or 0 if an error occurred.
+ * Return: If the function fails or filename is NULL - 0.
+ *         Otherwise, the actual number of bytes the function can read and print.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+size_t read_textfile(const char *filename, size_t letters)
 {
-	char *stream = malloc(letters * sizeof(char));
-	int file = open(filename, O_RDONLY);
+	size_t file_descriptor, bytes_read, bytes_written;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	if (file == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	ssize_t stream_read = read(file, stream, letters);
-	if (stream_read == -1)
+	file_descriptor = open(filename, O_RDONLY);
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (file_descriptor == -1 || bytes_read == -1 ||
+	    bytes_written == -1 || bytes_written != bytes_read)
 	{
-		free(stream);
-		close(file);
+		free(buffer);
 		return (0);
 	}
 
-	ssize_t stream_write = write(STDOUT_FILENO, stream, stream_read);
-	if (stream_write == -1 || stream_write != stream_read)
-	{
-		free(stream);
-		close(file);
-		return (0);
-	}
+	free(buffer);
+	close(file_descriptor);
 
-	free(stream);
-	close(file);
-
-	return (stream_write);
+	return (bytes_written);
 }
