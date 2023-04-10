d8e0a5a209a806139b3251731f1c4e3b5e1aee5b
Polygrapher1181 <moigowanjohi@gmail.com>
2023-04-10 13:57:13 -0700

    changes

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,9 +6,7 @@
  * @n: The number to print in binary
  */
 void print_binary(unsigned long int n)
-
 {
-    int count = 0 
     if (n == 0)
     {
         _putchar('0');
@@ -16,17 +14,13 @@ void print_binary(unsigned long int n)
     }
 
     int i;
-
     int size = sizeof(unsigned long int) * 8;
 
     for (i = size - 1; i >= 0; i--)
-
     {
         if ((n >> i) & 1)
             _putchar('1');
-
         else
             _putchar('0');
     }
 }
-
