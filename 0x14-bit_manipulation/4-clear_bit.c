2acd8222f07638fda038383bab4747ddd956655f
Polygrapher1181 <moigowanjohi@gmail.com>
2023-04-10 14:50:47 -0700

    fina attempt

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * clear bit - sets the value to 0
- *
- * @index: given index
- * Return 1 if correct -1 if wrong
+ * clear_bit - sets value of a bit to 0
+ * @n: the number
+ * @index: index of bit
+ * Return: 1 for success, or -1 for failure
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int pol = 1;
+	unsigned long int poll = 1;
 
 	if (index >= sizeof(unsigned long int) * 8)
+	{
 		return (-1);
+	}
 
-	pol <<= index;
 
-	if ((*n & pol) == pol)
-		*n ^= pol;
+	poll <<= index;
+
+	*n &= ~poll;
 
 	return (1);
 }
