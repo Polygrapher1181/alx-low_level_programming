6bb850cda565d26656577efdf98836f996c07ef9
Polygrapher1181 <moigowanjohi@gmail.com>
2023-04-14 17:02:26 -0700

    new changes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,4 @@
+#include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -110,8 +111,6 @@ void print_data(unsigned char *e_ident)
 	}
 }
 
-
-
 /**
  * print_version - Prints the version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
@@ -131,8 +130,6 @@ void print_version(unsigned char *e_ident)
 		break;
 	}
 }
-
-
 
 /**
  * print_osabi - Prints the OS/ABI of an ELF header.
@@ -179,7 +176,6 @@ void print_osabi(unsigned char *e_ident)
 	}
 }
 
-
 /**
  * print_abi - Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
@@ -194,9 +190,7 @@ void print_abi(unsigned char *e_ident)
  * print_type - Prints the type of an ELF header.
  * @e_type: The ELF type.
  * @e_ident: A pointer to an array containing the ELF class.
-*/
-
-
+ */
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
@@ -231,8 +225,6 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  * @e_entry: The address of the ELF entry point.
  * @e_ident: A pointer to an array containing the ELF class.
  */
-
-
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
 	printf("  Entry point address:               ");
@@ -303,7 +295,9 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		close_elf(o);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
-        check_elf(header->e_ident);
+	}
+
+	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
@@ -313,7 +307,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
-	}
 
-	return (0);
+	free(header);
+	close_elf(o);
+
 }
