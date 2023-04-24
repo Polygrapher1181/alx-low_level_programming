79cac38b9ae3833908efdf14f467e994dd42cb52
Polygrapher1181 <moigowanjohi@gmail.com>
2023-04-24 09:28:37 -0700

    last trial

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -297,6 +297,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
+	check_elf(header->e_ident);
+	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
 	print_data(header->e_ident);
